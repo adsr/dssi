a75fce38bbd1fa157e6e8d755c45b335a82a81e4
Chris Cannam <cannam@users.sourceforge.net>
2011-10-06 08:07:47 +0000

    Add --copy option to automake (don't want symlinks in packages)

--- autogen.sh
+++ autogen.sh
@@ -46,8 +46,8 @@ echo "=============== running libtoolize --force --copy" &&
     aclocal $ACLOCALARGS &&
     echo "=============== running autoheader" &&
     autoheader &&
-    echo "=============== running automake --add-missing --foreign" &&
-    automake --add-missing --foreign &&
+    echo "=============== running automake --add-missing --copy --foreign" &&
+    automake --add-missing --copy --foreign &&
     echo "=============== running autoconf" &&
     autoconf &&
     echo "=============== done"
